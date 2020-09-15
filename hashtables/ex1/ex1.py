5b4503bcf379ccc74a5f63ffc0f045fbff9ce074
Jeffrey <jeffreymartespolanco@hotmail.com>
2020-09-14 21:27:36 -0400

    Finished problem 1,2, and 4

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,17 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    data = {}
+    for i in range(len(weights)):
+        data[weights[i]] = i
+
+    for i in range(len(weights)):
+        difference = limit-weights[i]
+        
+        if difference in data:
+            return (max(i, data[difference]), min(i, data[difference]))
 
     return None
+
+
+
